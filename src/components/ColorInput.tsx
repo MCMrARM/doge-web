d8e2c3f23df507f29f2ebfba8472260b14cd3a53
MCMrARM <mcmrarm@gmail.com>
2020-09-06 23:41:24 +0200

    Nickname color picker now works

--- src/components/ColorInput.tsx
+++ src/components/ColorInput.tsx
@@ -3,10 +3,19 @@ import {Input} from "./Input";
 import "./ColorInput.sass";
 import {ExpandMoreIcon} from "../icons/Icons";
 import {ColorPicker} from "./ColorPicker";
+import hsvToRgb from "hsv-rgb";
+import rgbToHsv from "rgb-hsv";
+import {colorArrToString, parseColor} from "../util";
 
 export function ColorInput(props: { style?: CSSProperties, className?: string, value: string, placeholder?: string, onChange?: (value: string) => void }) {
     let [showPicker, setShowPicker] = useState(false);
     let [hsv, setHsv] = useState<[number, number, number]>([0, 0, 0]);
+    let hsvRgb = hsvToRgb(hsv[0], hsv[1], hsv[2]);
+    let parsedValue = parseColor(props.value);
+    if (parsedValue !== null && (parsedValue[0] !== hsvRgb[0] || parsedValue[1] !== hsvRgb[1] || parsedValue[2] !== hsvRgb[2])) {
+        hsv = rgbToHsv(parsedValue[0], parsedValue[1], parsedValue[2]);
+    }
+
     let ctrRef = createRef<HTMLDivElement>();
     let onFocus = () => {
         setShowPicker(true);
@@ -15,6 +24,11 @@ export function ColorInput(props: { style?: CSSProperties, className?: string, v
         if (!ctrRef.current!.contains(ev.relatedTarget as Node)) {
             setShowPicker(false);
         }
+    };
+    let onHsvChange = (value: [number, number, number]) => {
+        let rgb = hsvToRgb(value[0], value[1], value[2]);
+        setHsv(value);
+        props.onChange?.(colorArrToString(rgb));
     };
     return (
         <div ref={ctrRef} className={"ColorInput-ctr" + (showPicker ? " ColorInput-ctr-expanded" : "")}>
@@ -28,7 +42,7 @@ export function ColorInput(props: { style?: CSSProperties, className?: string, v
                 onBlur={onBlur} />
             <ExpandMoreIcon className="ColorInput-expandIcon" />
             {showPicker && (
-                <div className="ColorInput-picker" tabIndex={0} onBlur={onBlur}><ColorPicker hsv={hsv} onHsvChange={setHsv} /></div>
+                <div className="ColorInput-picker" tabIndex={0} onBlur={onBlur}><ColorPicker hsv={hsv} onHsvChange={onHsvChange} /></div>
             )}
         </div>
     );
--- src/util.ts
+++ src/util.ts
@@ -5,3 +5,11 @@ export function colorIntToHexString(color: number) {
 export function colorArrToString(arr: number[]) {
     return "#" + (arr[0].toString(16).padStart(2, "0")) + (arr[1].toString(16).padStart(2, "0")) + (arr[2].toString(16).padStart(2, "0"));
 }
+
+export function parseColor(str: string): [number, number, number, number] | null {
+    if (str.match(/$#[0-9a-fA-F]{8}^/)) {
+        let rgb = parseInt(str.substr(1), 16);
+        return [(rgb >> 16) & 0xFF, (rgb >> 8) & 0xFF, (rgb >> 0) & 0xFF, 255];
+    }
+    return null;
+}
