a01d80f5d998be271d6bfd5fa0ae333932a1feea
MCMrARM <mcmrarm@gmail.com>
2020-09-27 21:18:02 +0200

    Mobile (tablet) improvements

--- src/admin/AdminMain.tsx
+++ src/admin/AdminMain.tsx
@@ -1,6 +1,6 @@
-import React, {FocusEvent, useEffect, useRef, useState} from 'react';
+import React, {FocusEvent, ReactNode, ReactNodeArray, useEffect, useLayoutEffect, useRef, useState} from 'react';
 import './AdminMain.sass';
-import {NavLink, Link, Route, Switch, useParams, Redirect} from "react-router-dom";
+import {NavLink, Link, Route, Switch, useParams, Redirect, useLocation} from "react-router-dom";
 import {
     ContactIcon,
     DashboardIcon,
@@ -8,7 +8,7 @@ import {
     ExpandMoreIcon,
     HistoryEduIcon,
     SecurityIcon,
-    AccessTimeIcon
+    AccessTimeIcon, CloseIcon
 } from "../icons/Icons";
 import {Overview} from "./Overview";
 import {Leveling} from "./Leveling";
@@ -73,6 +73,7 @@ function GuildListDropdown(props: {list: GuildListEntry[]}) {
 
 export function AdminMain() {
     const dispatch = useDispatch();
+    const location = useLocation();
     let {id} = useParams<{id: string}>();
 
     useEffect(() => {
@@ -127,15 +128,41 @@ export function AdminMain() {
     }, [guildList, dispatch]);
     let serverListDropDown = serverListExpanded && <GuildListDropdown list={guildList.list || []} />;
 
+    const [mobileSidebarVisible, setMobileSidebarVisible] = useState(false);
+    const mobileHeaderComponent = (props: {to: string, exact?: boolean, children: ReactNode|ReactNode[]}) => {
+        return (
+            <Route path={props.to} exact={props.exact}>
+                {props.children}
+            </Route>
+        );
+    };
+
+    useLayoutEffect(() => {
+        setMobileSidebarVisible(false);
+    }, [location]);
+
     return (
         <div>
             <div className="AdminMain">
-                <div className="AdminMain-sidebar">
+                <div className="AdminMain-mobileHeader">
                     <div className="AdminName-sidebar-server" onClick={() => setServerListExpanded(!serverListExpanded)} onBlur={onServerListBlur} tabIndex={0}>
                         <img src={rServerInfo?.info?.iconUrl || "data:image/svg+xml;charset=utf8,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%3E%3C/svg%3E"} alt="Server Icon" className="Icon" />
                         {rServerInfo?.info?.name || "Server"}
                         <ExpandMoreIcon className="AdminName-sidebar-server-expand" />
                         {serverListDropDown}
+                    </div>
+                    <div className="AdminMain-mobileHeader-current" onClick={() => setMobileSidebarVisible(true)}>
+                        <AdminMainLinkList id={id} component={mobileHeaderComponent} />
+                        <ExpandMoreIcon className="AdminMain-mobileHeader-current-expand" />
+                    </div>
+                </div>
+                <div className={"AdminMain-sidebar" + (mobileSidebarVisible ? " AdminMain-sidebar-mobileVisible" : "")}>
+                    <div className="AdminName-sidebar-server" onClick={() => setServerListExpanded(!serverListExpanded)} onBlur={onServerListBlur} tabIndex={0}>
+                        <img src={rServerInfo?.info?.iconUrl || "data:image/svg+xml;charset=utf8,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%3E%3C/svg%3E"} alt="Server Icon" className="Icon" />
+                        {rServerInfo?.info?.name || "Server"}
+                        <ExpandMoreIcon className="AdminName-sidebar-server-expand" />
+                        {serverListDropDown}
+                        <div className="AdminName-sidebar-mobileClose" onClick={(ev) => { setMobileSidebarVisible(false); ev.stopPropagation(); }}><CloseIcon /></div>
                     </div>
                     <nav>
                         <ul>
@@ -156,6 +183,21 @@ export function AdminMain() {
             <p className="AdminMain-bottomtext">Thank you for using Doge!</p>
         </div>
     );
+}
+
+function AdminMainLinkList(props: {component: React.ComponentType<{to: string, exact?: boolean, children: ReactNode|ReactNode[]}>, id: string}) {
+    const {id} = props;
+    return (
+        <React.Fragment>
+            <props.component to={`/${id}/admin`} exact={true}><DashboardIcon className="Icon"/> Overview</props.component>
+            <props.component to={`/${id}/admin/embed`}><DashboardIcon className="Icon"/> Embed Studio</props.component>
+            <props.component to={`/${id}/admin/xp`}><EmojiEventsIcon className="Icon"/> Leveling</props.component>
+            <props.component to={`/${id}/admin/log`}><HistoryEduIcon className="Icon"/> Logging</props.component>
+            <props.component to={`/${id}/admin/welcome`}><ContactIcon className="Icon"/> Welcome Card</props.component>
+            <props.component to={`/${id}/admin/role`}><AccessTimeIcon className="Icon"/> Saved & Timed Roles</props.component>
+            <props.component to={`/${id}/admin/permission`}><SecurityIcon className="Icon"/> Permissions</props.component>
+        </React.Fragment>
+    )
 }
 
 function AdminMainRouter(props: {server: ServerInfo, config: BotConfig}) {
